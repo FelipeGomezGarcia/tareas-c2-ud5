035ad2acae3f3fdd63ac43be4ccd763d8ef39529
FelipeGomezGarcia <feli.goga@gmail.com>
2022-07-07 09:46:59 +0200

    Actividad 1

---
+++ Actividad1/src/Actividad1App.java
@@ -0,0 +1,14 @@
+
+public class Actividad1App {
+	public static void main (String args[]) {
+		int x = 30 , y = 30;
+		
+		if (x > y) {
+			System.out.println(x + " es mayor");
+		}else if (y > x) {
+			System.out.println(y + " es mayor");
+		}else {
+			System.out.println("Los numeros son iguales");
+		}
+	}
+}
